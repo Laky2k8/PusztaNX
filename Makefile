f03e9a4ddc2030a9978b3e305e49f468f6fbc265
Laky2k8 <laky2k8@gmail.com>
2026-07-19 11:22:34 +0200

    getting websites via curl

--- Makefile
+++ Makefile
@@ -61,7 +61,7 @@ ROMFS		:=	romfs
 ARCH	:=	-march=armv8-a+crc+crypto -mtune=cortex-a57 -mtp=soft -fPIE
 
 CFLAGS	:=	-g -Wall -O2 -ffunction-sections \
-			$(ARCH) $(DEFINES)
+			$(ARCH) $(DEFINES) 
 
 CFLAGS	+=	$(INCLUDE) -D__SWITCH__
 
@@ -70,7 +70,7 @@ CXXFLAGS	:= $(CFLAGS) -fno-rtti -fno-exceptions
 ASFLAGS	:=	-g $(ARCH)
 LDFLAGS	=	-specs=$(DEVKITPRO)/libnx/switch.specs -g $(ARCH) -Wl,-Map,$(notdir $*.map)
 
-LIBS	:= -lraylib -lEGL -lGLESv2 -lglapi -ldrm_nouveau -lnx -lm
+LIBS	:= -lcurl -lraylib -lEGL -lGLESv2 -lglapi -lz -ldrm_nouveau -lnx -lm 
 
 #---------------------------------------------------------------------------------
 # list of directories containing libraries, this must be the top level containing
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string>
+#include <optional>
 
 #include <raylib.h>
 #include <switch.h>
+
+
+#include <curl/curl.h>
 
 const int screenWidth = 1280;
 const int screenHeight = 720;
@@ -17,6 +21,9 @@ const float leftTriggerDeadzone = -0.9f;
 const float rightTriggerDeadzone = -0.9f;
 
 int gamepad = 0; // gamepad index
+bool running = true;
+
+int request_website(std::string url, std::string* htmlResponse);
 
 int main(void)
 {
@@ -37,15 +44,25 @@ int main(void)
         EndDrawing();
     }
 
-    while (!WindowShouldClose())
+    std::string websiteHTML = "";
+    int requestRes = request_website("https://example.org", &websiteHTML);
+
+    while (!WindowShouldClose() && running)
     {
         BeginDrawing();
         
             if(IsGamepadAvailable(gamepad))
             {
+
+                // Plus button to exit
+                if (IsGamepadButtonPressed(gamepad, GAMEPAD_BUTTON_MIDDLE_RIGHT))
+                {
+                    running = false;
+                }
+
                 ClearBackground(RAYWHITE);
 
-                DrawText(TextFormat("GP%d: %s", gamepad, GetGamepadName(gamepad)), 10, 10, 20, BLACK);
+                //DrawText(TextFormat("GP%d: %s", gamepad, GetGamepadName(gamepad)), 10, 10, 20, BLACK);
 
                 // Get axis values
                 float leftStickX = GetGamepadAxisMovement(gamepad, GAMEPAD_AXIS_LEFT_X);
@@ -64,13 +81,16 @@ int main(void)
                 if (rightTrigger < rightTriggerDeadzone) rightTrigger = -1.0f;
 
 
-                DrawText("Puszta Browser NX", 190, 200, 20, BLACK);
-                DrawText("By Laky2k8", 190, 230, 20, RED);
+                DrawText("Puszta Browser NX", 20, 40, 20, BLACK);
+                DrawText("By Laky2k8", 20, 80, 20, RED);
 
-                DrawText(TextFormat("Left Stick: (%.2f, %.2f)", leftStickX, leftStickY), 10, 50, 20, DARKGRAY);
-                DrawText(TextFormat("Right Stick: (%.2f, %.2f)", rightStickX, rightStickY), 10, 80, 20, DARKGRAY);
-                DrawText(TextFormat("Left Trigger: %.2f", leftTrigger), 10, 110, 20, DARKGRAY);
-                DrawText(TextFormat("Right Trigger: %.2f", rightTrigger), 10, 140, 20, DARKGRAY);
+                DrawText(TextFormat("Result code of request: %d", requestRes), 1020, 50, 20, BLACK);
+                DrawText(websiteHTML.c_str(), 1020, 80, 20, BLACK);
+
+                DrawText(TextFormat("Left Stick: (%.2f, %.2f)", leftStickX, leftStickY), 1020, 590, 20, DARKGRAY);
+                DrawText(TextFormat("Right Stick: (%.2f, %.2f)", rightStickX, rightStickY), 102, 620, 20, DARKGRAY);
+                DrawText(TextFormat("Left Trigger: %.2f", leftTrigger), 1020, 650, 20, DARKGRAY);
+                DrawText(TextFormat("Right Trigger: %.2f", rightTrigger), 1020, 680, 20, DARKGRAY);
 
                 //DrawTexture(exampleTex, screenWidth/2 - exampleTex.width/2, screenHeight/2 - exampleTex.height/2, WHITE);
                 //DrawTexture(exampleTex, screenWidth/2 - exampleTex.width/2, 300, WHITE);
@@ -81,4 +101,81 @@ int main(void)
     CloseWindow();
 
     return 0;
+}
+
+size_t write_to_string(void * a_buffer, size_t a_size, size_t a_nmemb, void * a_userp)
+{
+    size_t resulting_size = a_size * a_nmemb;
+
+    std::string * result_string = static_cast<std::string *>(a_userp);
+    char * buffer_char_ptr = static_cast<char *>(a_buffer);
+
+    // Check for overflow
+    if(result_string->max_size() - result_string->size() < resulting_size)
+    {
+        return 0;
+    }
+
+    result_string->append(buffer_char_ptr, resulting_size);
+
+    return resulting_size;
+}
+
+int request_website(std::string url, std::string* htmlResponse)
+{
+    CURL *curl;
+    CURLcode resCode;
+    char curl_errbuf[CURL_ERROR_SIZE];
+    std::string response;
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+
+    DrawText("Initalizing curl...", 10, 90, 20, BLUE);
+    resCode = curl_global_init(CURL_GLOBAL_ALL);
+
+    if(resCode != CURLE_OK)
+    {
+        DrawText(TextFormat("curl init failed: %d", (int)resCode), 10, 120, 20, RED);
+        return (int)resCode;
+    }
+
+    curl = curl_easy_init();
+
+    if(curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, curl_errbuf);
+        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0L);
+        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36");
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        DrawText("Getting webpage", 10, 90, 20, BLUE);
+        resCode = curl_easy_perform(curl);
+
+        if(resCode == CURLE_OK)
+        {
+            *htmlResponse = response;
+        }
+        else
+        {
+            DrawText(TextFormat("Getting webpage failed: %s", curl_errbuf), 10, 150, 20, RED);
+        }
+
+        curl_easy_cleanup(curl);
+
+    }
+    else
+    {
+       DrawText(TextFormat("curl init failed: %s", curl_errbuf), 10, 120, 20, RED);
+    }
+
+    curl_global_cleanup();
+    EndDrawing();
+
+    return (int)resCode;
 }
